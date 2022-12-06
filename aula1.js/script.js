f3f7ec5267b1ff4d0b14ef490da2954843e5c657
Maria Carolina <demycar24@gmail.com>
2022-12-05 22:04:29 -0300

    commit inicial

---
+++ aula1.js/script.js
@@ -0,0 +1,8 @@
+let num1 = prompt("Digite um número:");
+let num2 = prompt("Digite um número:");
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+const result = num1 + num2;
+alert("O resultado foi:" + result);

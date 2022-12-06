c9ebdaa66bf1b0b85e2a8058cfb0e8b9917a785b
Maria Carolina <demycar24@gmail.com>
2022-12-06 13:20:36 -0300

    variaveis

---
+++ aula3/script.js
@@ -0,0 +1,6 @@
+let nome = "Maria Carolina";
+
+console.log(nome, "é um menina muito estudiosa!");
+console.log("Tenho certeza que a", nome, "será uma programadora muito boaa!!");
+console.log("Parabén", nome, "você vai muito longe!");
+console.log(nome, "só continua que vai dar certo!");

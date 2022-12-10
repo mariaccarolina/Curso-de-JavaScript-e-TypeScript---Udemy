9ae2d987c58c476cefd798a729ed73e199531277
Maria Carolina <demycar24@gmail.com>
2022-12-09 23:28:15 -0300

    exercicios

---
+++ aula9/script.js
@@ -0,0 +1,9 @@
+const nome = prompt("Digite seu nome completo:");
+document.body.innerHTML += `Seu nome é: <strong>${nome}</strong> <br/>`;
+document.body.innerHTML += `Seu nome tem ${nome.length} letras <br/>`;
+document.body.innerHTML += `A segunda letra do seu nome é: ${nome[0]} <br/>`;
+document.body.innerHTML += `Qual o primeiro índice da letra LETRA no seu nome?  ${nome.indexOf('a')}<br/>`;
+document.body.innerHTML += `Qual o último índice da letra LETRA no seu nome?  ${nome.lastIndexOf('a')} <br/>`;
+document.body.innerHTML += `As últimas 3 letras do seu nome são: ${nome.slice(2)} <br/>`;
+document.body.innerHTML += `Seu nome com letras maiúsculas: ${nome.toUpperCase()} <br/>`;
+document.body.innerHTML += `Seu nome com letras minúsculas:${nome.toLowerCase()}  <br/>`;

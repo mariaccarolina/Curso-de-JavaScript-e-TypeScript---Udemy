8f1da4a412d3b3ae9e984c629ca90f50ae394084
Maria Carolina <demycar24@gmail.com>
2022-12-18 00:00:22 -0300

    funcões

---
+++ aula13/script.js
@@ -0,0 +1,14 @@
+function soma(x, y) {
+  const result = x + y;
+  return result;
+}
+
+//função anônima
+const raiz = function (n) {
+return n ** 0.5;
+};
+
+//arrow function
+const divisao = (x, y) => {
+  return x / y;
+};

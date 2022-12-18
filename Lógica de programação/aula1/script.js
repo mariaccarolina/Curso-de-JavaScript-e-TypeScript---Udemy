c606df9ff03b3dd1635ee98b5d335b4bb604dbd3
Maria Carolina <demycar24@gmail.com>
2022-12-18 13:51:55 -0300

    if else

---
+++ Lógica de programação/aula1/script.js
@@ -0,0 +1,11 @@
+// Entre 0 e 11 vai falar bom dia
+
+let hora = 24;
+
+if (hora >= 0 && hora <= 11) {
+  console.log("Bom dia!");
+}else if(hora >= 12 && hora <= 17){
+    console.log("Boa tarde!")
+}else{
+    console.log("Boa noite!")
+}

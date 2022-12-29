7dd9bab4b0a87722f20291ddbb716f6255cf2194
Maria Carolina <demycar24@gmail.com>
2022-12-29 00:28:04 -0300

    operação ternária

---
+++ script.js
@@ -0,0 +1,12 @@
+// (condicao) ? "valor para verdadeiro" : "valor para falso";
+
+const pontuacaoUsuario = 999;
+const nivelUsuario =
+  pontuacaoUsuario >= 1000 ? "Usuário VIP!" : "Usuario normal";
+console.log(nivelUsuario);
+
+//if (pontuacaoUsuario >= 1000) {
+// console.log("Usuário VIP");
+//} else {
+// console.log("Usuário normal");
+//}

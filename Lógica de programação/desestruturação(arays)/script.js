b36f8a350381114337626133e46a534b1c443cae
Maria Carolina <demycar24@gmail.com>
2022-12-29 01:54:40 -0300

    add-atualizaçoes

---
+++ Lógica de programação/desestruturação(arays)/script.js
@@ -0,0 +1,9 @@
+const numeros = [18, 200, 3, 4, 5];
+const [batata, segundoNumero] = numeros;
+console.log(batata, segundoNumero);
+
+//                 0         1         2
+//              1  2  3   4  5  6   7  8  9
+const teste = [[1, 2, 3],[4, 5, 6],[7, 8, 9]];
+const [, [, , seis]] = teste;
+console.log(seis);

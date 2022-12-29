a4e79484c22ae273515e6c2639e0b7a064b01302
Maria Carolina <demycar24@gmail.com>
2022-12-29 01:58:16 -0300

    arrays

--- Lógica de programação/desestruturação(arays)/script.js
+++ Lógica de programação/desestruturação(arays)/script.js
@@ -5,5 +5,8 @@ console.log(batata, segundoNumero);
 //                 0         1         2
 //              1  2  3   4  5  6   7  8  9
 const teste = [[1, 2, 3],[4, 5, 6],[7, 8, 9]];
+//const [lista1, lista2, lista3] = teste;
+//console.log(lista2[2])
 const [, [, , seis]] = teste;
 console.log(seis);
+

caaacdeb10e470475b3fb546dbe94dedddc87ffb
Maria Carolina <demycar24@gmail.com>
2022-12-29 22:55:53 -0300

    exercicio

---
+++ Lógica de programação/exercicio.js/script.js
@@ -0,0 +1,11 @@
+//Escreva uma função que recebe 2 números e retorna o maior;
+
+function maiorNumero(num1, num2) {
+  return num1 > num2 ? num1 : num2;
+  //if (num1 > num2) {
+  //return num1;
+  // } else {
+  //   return num2;
+  // }
+}
+console.log(maiorNumero(78, 155));
